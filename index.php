c33a1f8fcc5592e3846d824b5f5c8dec2c98e6d2
Thyago Assunção <thyago.brejao@gmail.com>
2018-09-13 15:39:36 -0300

    Third

--- index.php
+++ index.php
@@ -1,8 +1,4 @@
 <?php
-require "vendor/autoload.php";
 
-use Carbon\Carbon;
-
-
-echo "<h1>Openshift Workshop v1.0 " . Carbon::now() . "</h1>";
+echo "<h1>Openshift Workshop v1.0 </h1>";
 echo $_SERVER['SERVER_ADDR'];
